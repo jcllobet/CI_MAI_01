349a0aac164e7db438884e156763bae4a48221d5
Jan Carbonell <jcllobet@gmail.com>
2018-11-26 15:24:02 +0100

    initial template from class

---
+++ neural_networks_class_guide.m
@@ -0,0 +1,20 @@
+% Neural Networks Class Guide
+
+%% Define the neuron parameters
+% Neuron weights
+w = [4 -2];
+% Neuron bias
+b = -3;
+% Activation function: Hyperbolic tangent sigmoid function
+func = 'tansig';
+% Activation function: Logistic sigmoid transfer function
+% func = 'logsig'
+% Activation function: Hard-limit transfer function (threshold)
+% func = 'hardlim'
+% Activation function: Linear transfer function
+% func = 'purelin'
+
+%% Define input vectors
+p = [2 3]
+
+%% Calculate neuron output
